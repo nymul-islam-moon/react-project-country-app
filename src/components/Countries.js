dd2db7fa33b3c4fa0604bffebdb344245275b32e
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-19 10:12:36 +0600

    Country App Styling DOne

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -2,14 +2,17 @@ import React from 'react';
 
 import { v4 as uuidv4 } from "uuid";
 import Country from './Country';
+import style from "./countries.module.css";
 
 const Countries = ( props ) => {
-    return <section>
-        {props.countries.map(( country ) => {
-            const countryNew = { country, id: uuidv4() }
-            return <Country { ...countryNew } key={ countryNew.id } />
-        })};
-    </section>
+    return (
+        <section className={style.countries}>
+            {props.countries.map(( country ) => {
+                const countryNew = { country, id: uuidv4() }
+                return <Country { ...countryNew } key={ countryNew.id } />
+            })}
+        </section>
+    )
 }
 
 export default Countries;
--- src/components/Country.js
+++ src/components/Country.js
@@ -1,18 +1,20 @@
 import React from 'react';
 
+import style from "./country.module.css";
+
 const Country = ( props ) => {
     const { country } = props;
     const { name, flags, capital, population, area } = country;
     return (
-        <artical>
+        <article className={style.country}>
             <div>
-                <img src={flags.png} alt={ name.common } />
+                <img src={flags.png} className={style.flag} alt={ name.common } />
                 <h3>Name : { name.common }</h3>
                 <h3>Population : { population }</h3>
                 <h3>Capital : { capital }</h3>
                 <h3>Area : { area }</h3>
             </div>
-        </artical>
+        </article>
     )
 }
 
